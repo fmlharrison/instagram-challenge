759c45f31ac9e469477c8b746fcdc71d70f837f2
Felix Harrison <fmlharrison@gmail.com>
2016-11-20 22:44:57 +0000

    added styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
 
+  before_action :set_post
+
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    authenticate_user!
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
     if @comment.save
       flash[:success] = "Comment added!"
       redirect_to post_path(@post)
@@ -12,9 +15,26 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    authenticate_user!
+    @comment = @post.comments.find(params[:id])
+    if current_user.id == @comment.user.id
+      @comment.destroy
+      flash[:success] = "Comment deleted..."
+      redirect_to root_path
+    else
+      flash[:alert] = "You are not the author of this comment! How dare you try to delete it!"
+      redirect_to post_path(@post)
+    end
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:comment)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
