74a3ed5c1569062192d6a7b003439fe0e9b08182
Felix Harrison <fmlharrison@gmail.com>
2016-11-20 11:25:28 +0000

    added more unit tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let!(:post) do
+    Post.create(image: File.new(Rails.root + 'spec/fixtures/files/head_shot_copy_3.jpg'))
+  end
+
+  describe 'validations' do
+
+    it 'has a comment' do
+      comment = Comment.new(comment: "This is a great photo", post_id: post.id)
+      expect(comment).to be_valid
+    end
+
+    it 'doesn\'t have a comment' do
+      comment = Comment.new(comment: "", post_id: post.id)
+      expect(comment).not_to be_valid
+    end
+  end
+
+  describe 'table relationships' do
+
+    it 'belongs to post' do
+      c = Comment.reflect_on_association(:post)
+      expect(c.macro).to eq :belongs_to
+    end
+
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,7 +8,14 @@ RSpec.describe Post, type: :model do
       post = Post.new :image => File.new(Rails.root + 'spec/fixtures/files/head_shot_copy_3.jpg')
       expect(post).to be_valid
     end
-    
+
+  end
+
+  describe 'table relationships' do
+    it 'has many comments' do
+      p = Post.reflect_on_association(:comments)
+      expect(p.macro).to eq :has_many
+    end
   end
 
 end
